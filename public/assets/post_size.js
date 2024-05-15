21814978a4e69b5cfceb00e593d3b1cb94f4d90f
Elena <110896958+Elena-web@users.noreply.github.com>
2024-05-15 12:33:02 +0300

    добавила файл yml

--- public/assets/post_size.js
+++ public/assets/post_size.js
@@ -1,36 +1,35 @@
- const posSize = (str) => {
-    const wordsArr = str.split(' ');
-    const domains = {
-        1: '.org',
-        2: '.com',
-        3: '.ru',
-        4: '.net',
-        5: '.gov',
-        6: '.edu',
-        7: '.mil',
-        8: '.cn',
-        9: '.by',
-        10: '.info',
-        11: '.biz',
-        12: 'www.',
-        13: 'http://',
-        14: 'https://',
-    };
-    for (let key in domains) {
-        for (let i = 0; i < wordsArr.length; i++) {
-          if (wordsArr[i].includes(domains[key])) {
-            if (i === wordsArr.length - 1) {
-              // Заменяю последний элемент на пробел
-              wordsArr[i] = '';
-            } else {
-              // Удаляю элемент с индексом i
-              wordsArr.splice(i, 1);
-              i--;
-            }
-          }
+export default function postSize(str) {
+  const wordsArr = str.split(' ');
+  const domains = {
+    1: '.org',
+    2: '.com',
+    3: '.ru',
+    4: '.net',
+    5: '.gov',
+    6: '.edu',
+    7: '.mil',
+    8: '.cn',
+    9: '.by',
+    10: '.info',
+    11: '.biz',
+    12: 'www.',
+    13: 'http://',
+    14: 'https://',
+  };
+  for (let j = 0; j < domains.length; j += 1) {
+    for (let i = 0; i < wordsArr.length; i += 1) {
+      if (wordsArr[i].includes(domains[j])) {
+        if (i === wordsArr.length - 1) {
+          // Заменяю последний элемент на пробел
+          wordsArr[i] = '';
+        } else {
+          // Удаляю элемент с индексом i
+          wordsArr.splice(i, 1);
+          i -= 1;
         }
       }
-    const result = wordsArr.join(' ');
-    console.log(result.length);
+    }
+  }
+  const result = wordsArr.join(' ');
+  return result.length;
 }
-export default posSize;
