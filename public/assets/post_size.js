d65035be7c5fa068e4397c7134d01ed64e30c63c
Elena <110896958+Elena-web@users.noreply.github.com>
2024-05-18 15:06:52 +0300

    добавила тесты к функции подсчёта символов

--- public/assets/post_size.js
+++ public/assets/post_size.js
@@ -1,6 +1,11 @@
 export default function postSize(str) {
   const wordsArr = str.split(' ');
-  const domains = {
+  const protocolNames = {
+    1: 'www.',
+    2: 'http://',
+    3: 'https://',
+  };
+  const domainsNames = {
     1: '.org',
     2: '.com',
     3: '.ru',
@@ -12,20 +17,29 @@ export default function postSize(str) {
     9: '.by',
     10: '.info',
     11: '.biz',
-    12: 'www.',
-    13: 'http://',
-    14: 'https://',
   };
-  for (let j = 0; j < domains.length; j += 1) {
-    for (let i = 0; i < wordsArr.length; i += 1) {
-      if (wordsArr[i].includes(domains[j])) {
-        if (i === wordsArr.length - 1) {
-          // Заменяю последний элемент на пробел
-          wordsArr[i] = '';
-        } else {
-          // Удаляю элемент с индексом i
-          wordsArr.splice(i, 1);
-          i -= 1;
+
+  for (let j = 0; j < Object.keys(protocolNames).length; j += 1) {
+    for (let k = 0; k < Object.keys(domainsNames).length; k += 1) {
+      for (let i = 0; i < wordsArr.length; i += 1) {
+        let slicedWord = wordsArr[i];
+        if (wordsArr[i].includes(protocolNames[j]) && wordsArr[i].includes(domainsNames[k])) {
+          const startIndex = wordsArr[i].indexOf(protocolNames[j]);
+          const endIndex = wordsArr[i].lastIndexOf(domainsNames[k]) + domainsNames[k].length - 1;
+          if (startIndex !== -1 && endIndex !== -1) {
+            slicedWord = wordsArr[i].substring(0, startIndex) + wordsArr[i].substring(endIndex + 1);
+          }
+          wordsArr[i] = slicedWord;
+        } else if (wordsArr[i].includes(domainsNames[k])
+          && (!wordsArr[i].includes(protocolNames[1])
+          && !wordsArr[i].includes(protocolNames[2])
+          && !wordsArr[i].includes(protocolNames[3]))
+          && (wordsArr[i].length > domainsNames[k].length)) {
+          const endIndex = wordsArr[i].lastIndexOf(domainsNames[k]) + domainsNames[k].length - 1;
+          if (endIndex !== -1) {
+            slicedWord = wordsArr[i].substring(endIndex + 1);
+          }
+          wordsArr[i] = slicedWord;
         }
       }
     }
