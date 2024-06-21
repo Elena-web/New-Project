e2413c619d338b9530c0c2e45d0396ab4ffffd82
Elena <110896958+Elena-web@users.noreply.github.com>
2024-06-21 19:35:04 +0300

    внесла исправления в функцию

--- public/assets/highlight_hashtags.js
+++ public/assets/highlight_hashtags.js
@@ -1,11 +1,17 @@
 export default function highlightHashtags(text) {
   const words = text.split(' ');
+  const result = [];
+
   for (let i = 0; i < words.length; i += 1) {
-    if (words[i][0] === '#') {
-      const str = words[i].substring(1);
-      words[i] = `<a href="/search?tag=${str}">${words[i]}</a>`;
+    const word = words[i];
+    if (word.startsWith('#')) {
+      const hashtags = word.split('#').filter(Boolean);
+      const links = hashtags.map((hashtag) => `<a href="/search?tag=${encodeURIComponent(hashtag)}">#${hashtag}</a>`);
+      result.push(links.join(''));
+    } else {
+      result.push(word);
     }
   }
-  const newText = words.join(' ');
-  return newText;
+
+  return result.join(' ');
 }
