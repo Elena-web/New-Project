eb72fcac73fbce063b2330e6da2b5745e19d6e72
Elena <110896958+Elena-web@users.noreply.github.com>
2024-10-15 11:56:39 +0300

    настроила express

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Получаем путь к текущему файлу
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
 
 const app = express();
 const port = 3000;
 
-app.use(express.static('public'));
+// Указываем, что статические файлы находятся в папке public
+app.use(express.static(path.join(dirname, 'public')));
 
+// Определение маршрута для главной страницы
+app.get('/', (req, res) => {
+  res.sendFile(path.join(dirname, 'index.html'));
+});
+
+// Запуск сервера
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Server is running at http://localhost:${port}`);
 });
