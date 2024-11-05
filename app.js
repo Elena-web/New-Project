6b68c3f120c796a93e7d433e0a0d7f824d382bce
Elena <110896958+Elena-web@users.noreply.github.com>
2024-11-05 13:57:18 +0300

    исправила пути в app.js

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 import express from 'express';
 import path from 'path';
+import { fileURLToPath } from 'url';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-app.use(express.static('public'));
+// Получаем путь к текущему файлу
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
+
 // Основной маршрут
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(dirname, 'index.html')); // Отправляем index.html из корня проекта
 });
+
+// Запуск сервера
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Сервер запущен на порту ${port}`);
 });
