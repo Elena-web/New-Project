5fe11b91a8ab56d4d60ae03942ddf71e12da6935
Elena <110896958+Elena-web@users.noreply.github.com>
2024-11-05 13:45:34 +0300

    внесла изменения в app.js

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 import express from 'express';
+import path from 'path';
 
 const app = express();
 const port = 3000;
 
 app.use(express.static('public'));
-
+// Основной маршрут
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
