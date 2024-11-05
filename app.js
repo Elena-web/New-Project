637714d901d29284031134886504a185f0b82303
Elena <110896958+Elena-web@users.noreply.github.com>
2024-11-05 16:41:28 +0300

    изменила app.js

--- app.js
+++ app.js
@@ -1,20 +1,10 @@
 import express from 'express';
-import path from 'path';
-import { fileURLToPath } from 'url';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Получаем путь к текущему файлу
-const filename = fileURLToPath(import.meta.url);
-const dirname = path.dirname(filename);
+app.use(express.static('public'));
 
-// Основной маршрут
-app.get('/', (req, res) => {
-  res.sendFile(path.join(dirname, 'index.html')); // Отправляем index.html из корня проекта
-});
-
-// Запуск сервера
 app.listen(port, () => {
-  console.log(`Сервер запущен на порту ${port}`);
+  console.log(`Example app listening on port ${port}`);
 });
